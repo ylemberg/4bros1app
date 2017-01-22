6c22093347b806b2749fad1ae30cdb53f08fa8e4
Daniel Osaki <osaki.daniel@gmail.com>
2017-01-21 22:19:21 -0800

    add basic modals

--- public/app/index.jsx
+++ public/app/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
 import MovieList from './movieList.jsx'
+import { Modal } from 'react-bootstrap';
+import { DropdownButton } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
+
 
 class App extends React.Component {
 	constructor(props) {
     super(props)
     this.state = {
-      movies: ['movieObject1', 'movieObject2']
+      movies: ['movieObject1', 'movieObject2'],
+      showSearchModal: false,
+      showSuggestModal: false
     }
-    this.search = this.search.bind(this)
-    this.suggest = this.suggest.bind(this)
+    this.openSearch = this.openSearch.bind(this)
+    this.closeSearch = this.closeSearch.bind(this)
+    this.openSuggest = this.openSuggest.bind(this)
+    this.closeSuggest = this.closeSuggest.bind(this)
   }
 
   componentDidMount () {
@@ -25,31 +33,80 @@ class App extends React.Component {
    //    console.log('error in component did mount catlist', err)
    //  })
   }
-
-  search() {
-
+  openSearch() {
+    this.setState({ showSearchModal: true });
   }
 
-  suggest() {
+  closeSearch() {
+    this.setState({ showSearchModal: false });
+  }
 
+  openSuggest() {
+    this.setState({showSuggestModal: true})
+  }
+
+  closeSuggest() {
+    this.setState({showSuggestModal: false})
   }
 
   render () {
     return(
     	<div>
   		  <button onClick ={()=> {
-            this.search()
+            this.openSearch()
           }}>
     		Search
     		</button>
 
-    		<button onClick ={()=> {
-            this.suggest()
-          }}>
-    		Pick a Flick
-    		</button>
+        <Modal show={this.state.showSearchModal} onHide={this.closeSearch}>
+          <Modal.Header closeButton>
+            <Modal.Title>Find a film!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src = {'https://cdn-webimages.wimages.net/050f7fd026f91831576663f886214fb772dc6-wm.jpg?v=3'}/>
+          </Modal.Body>
+        </Modal>
 
-    		<MovieList 
+        <button onClick ={()=> {
+            this.openSuggest()
+          }}>
+        Pick a Flick
+        </button>
+
+        <Modal show={this.state.showSuggestModal} onHide={this.closeSuggest}>
+          <Modal.Header closeButton>
+            <Modal.Title>Let us help you!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Fill out some quiz</p>
+            <p>Which genre?</p>
+            <DropdownButton title={'genre'} id={'askGenre'}>
+              <MenuItem eventKey="1">Action</MenuItem>
+              <MenuItem eventKey="2">Comedy</MenuItem>
+              <MenuItem eventKey="3">Drama</MenuItem>
+              <MenuItem eventKey="4">Romance</MenuItem>
+              <MenuItem eventKey="5">Indifferent</MenuItem>
+            </DropdownButton>
+            <p>Which era?</p>
+            <DropdownButton title={'era'} id={'askEra'}>
+              <MenuItem eventKey="1">Classic</MenuItem>
+              <MenuItem eventKey="2">Modern</MenuItem>
+              <MenuItem eventKey="3">New</MenuItem>
+              <MenuItem eventKey="4">Old</MenuItem>
+              <MenuItem eventKey="5">Indifferent</MenuItem>
+            </DropdownButton>
+            <p>Sort by?</p>
+            <DropdownButton title={'sort'} id={'askSort'}>
+              <MenuItem eventKey="1">Awards</MenuItem>
+              <MenuItem eventKey="2">Rating</MenuItem>
+              <MenuItem eventKey="3">Popularity</MenuItem>
+              <MenuItem eventKey="4">Recommended</MenuItem>
+              <MenuItem eventKey="5">Indifferent</MenuItem>
+            </DropdownButton>
+          </Modal.Body>
+        </Modal>
+
+        <MovieList 
     			movies = {this.state.movies}
     		/>
 
@@ -57,5 +114,7 @@ class App extends React.Component {
     )
   }
 }
+
+
 
 render(<App/>, document.getElementById('app'));
