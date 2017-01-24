41c4417fc0cb426bec04dd1fe10d6a9778cec706
jnocon <nocon.jesse@gmail.com>
2017-01-23 16:00:09 -0800

    added in search controller, node server controllers folder, corrected style errors on server.js, added imdbID and guideboxID to movieSchema

---
+++ api/controllers/searchController.js
@@ -0,0 +1,19 @@
+// require dependencies
+let mongoose = require('mongoose')
+let request = require('request-promise')
+let guidebox = require('guidebox')
+
+// require db models
+let Movie = require('../models/Movie.js')
+let User = require('../models/User.js')
+
+var searchController = {}
+
+searchController.byMovieTitle = (req, res) => {
+
+}
+
+
+
+
+module.exports = searchController
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-let express = require('express');
-let path = require('path');
-let bodyParser = require('body-parser');
-let routes = require('./api/routes/routes.js');
+let express = require('express')
+let path = require('path')
+let bodyParser = require('body-parser')
+let routes = require('./api/routes/routes.js')
 
 let port = 3000
-let app = express();
-app.use(function(req, res, next) {
-  console.log(req.method, req.url);
-  next();
-});
+let app = express()
+app.use(function (req, res, next) {
+  console.log(req.method, req.url)
+  next()
+})
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname + '/public')))
 
 // app.use('/node_modules', express.static(__dirname + "/node_modules"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/api', routes);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use('/api', routes)
 
 //start the server
 app.listen(port, () => {
-  console.log('listening on port ' + port);
-});
+  console.log('listening on port ' + port)
+})
 
-module.exports = app;
+module.exports = app
