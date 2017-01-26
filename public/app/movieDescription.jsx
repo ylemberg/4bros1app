90014c1659135f4395321a6997a2865d66bd545b
Richard Durazo <rcdurazo@gmail.com>
2017-01-25 17:33:27 -0800

    [feat] add icons to details fix parallax fix actors and genre spacing

--- public/app/movieDescription.jsx
+++ public/app/movieDescription.jsx
@@ -15,38 +15,65 @@ class movieDescription extends React.Component {
 		}
 	}
     render() {
+		var genres = (this.props.movie.genres).join(' ');
+		var actors = (this.props.movie.actors).join(' ');
+		var directors = (this.props.movie.directors).join(' ');
+
+		// var banner = function (ogBanner) {
+		// 	if (ogBanner === null) {
+		// 		ogBanner = 'public/images/cover.jpg';
+		// 	} else {
+		// 		ogBanner = ogBanner;
+		// 	}
+		// 	return ogBanner;
+		// }
+		// console.log(banner());
 	  return (<div><Parallax strength={300}>
           <Background>
-			<img src={this.props.movie.banner}/>
+			<img src={this.props.movie.banner} />
             <div style={{
                width: 800, 
                height: 300, 
               }}></div>
-			  <img src={this.props.movie.banner}/>
+			<img src={this.props.movie.banner} />
           </Background>
 	  <Col xs={6} md={4}>
-			<h4>genres: {this.props.movie.genres}</h4>
-	  <h4>staring: {this.props.movie.actors}</h4>
-	  <h4>directors: {this.props.movie.directors}</h4>
-	  
+
+	  <div style={{
+		   width: 800, 
+		   height: 300, 
+		}}></div>
 	  </Col>		  
 	  </Parallax>
 	  <Col xs={6} md={4}>
 	  <Image src={this.props.movie.poster} responsive />
+	  <h4>Genres: </h4><p>{genres}</p>
 
+	  <h4>Directors: </h4><p>{directors}</p>
+	  <h4>Where to Watch: </h4>
+	  <Image src="/images/hbo.png" style={{
+		  height: 20, 
+		  margin: 5}}/>
+		<Image src="/images/Hulu_Logo_Option_A.png" style={{
+		  height: 20, 
+		  margin:5}}/>
+		  	<Image src="/images/netflix-logo-small.png" style={{
+		  height: 20, 
+		  margin:5}}/>
+		  <Image src="/images/amazon_logo.png" style={{
+		  height: 20, 
+		  margin:5}}/>
 	  </Col>
 	  <Col xs={12} md={8}>
 	  <PageHeader>{this.props.movie.title}<small> {this.props.movie.year}</small></PageHeader>
-	  <p>{this.props.movie.description}</p>  <h2>Trailer</h2>
+	  <p>{this.props.movie.description}</p>  <h3>Actors: </h3><p>{actors}</p>
+	  <h3>Trailer</h3>
 	  <div style={{width: 660, height: 'auto'}}>
 	  <div className="embed-responsive embed-responsive-4by3">
-	  <iframe className="embed-responsive-item" src={this.props.movie.trailer}></iframe>
+	  <iframe className="embed-responsive-item" src={this.props.movie.trailer} allowFullScreen></iframe>
 	  </div>
 	  </div>
-	  
-
 	  </Col>        
-
 	  </div>
 	  );
 	}
