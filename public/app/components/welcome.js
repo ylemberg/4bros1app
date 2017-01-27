beccf6be8616e412b72a89f9129ffdc3cae1a1a3
Richard Durazo <rcdurazo@gmail.com>
2017-01-26 22:16:07 -0800

    [prog] stack tech

--- public/app/components/welcome.js
+++ public/app/components/welcome.js
@@ -35,21 +35,37 @@ div style = {
     /div> <
     /Parallax> <div id="welcomeMiddle"style={{ marginTop: 200 }}>
 		<Image src="/images/react.png" style={{
-		  height: 80,
-		  margin:10,
-      marginTop:100}}/>
+      marginTop: 80,
+
+      display: 'block',
+      marginBottom: 30,
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      height: 80}}/>
     <Image src="https://webassets.mongodb.com/_com_assets/cms/MongoDB-Logo-5c3a7405a85675366beb3a5ec4c032348c390b3f142f5e6dddf1d78e2df5cb5c.png" style={{
-      height: 80,
-      margin: 10,
-      marginTop:100}}/>
+      display: 'block',
+      marginBottom: 30,
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      height: 80}}/>
     <Image src="http://mean.io/wp-content/themes/twentysixteen-child/images/express.png" style={{
-      height: 80,
-      margin:10,
-      marginTop:100}}/>
+      display: 'block',
+      marginBottom: 30,
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      height: 80}}/>
 		<Image src="/images/node.png" style={{
-		  height: 80,
-		  margin:10,
-      marginTop:100}}/>
+      display: 'block',
+      marginBottom: 30,
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      height: 80}}/>
+      <Image src="/images/passport.png" style={{
+        display: 'block',
+        marginBottom: 30,
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        height: 80}}/>
 
     <div style={{ margin: 200 }}>  </div>
       <Parallax strength={300}>
