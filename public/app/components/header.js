bcca301558c2276b17881e216f222232e1b5aaf8
Richard Durazo <rcdurazo@gmail.com>
2017-01-27 15:50:17 -0800

    [prog] navbar style and dropdown added

--- public/app/components/header.js
+++ public/app/components/header.js
@@ -1,33 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { Navbar } from 'react-bootstrap';
+
 
 class Header extends Component {
     renderLinks() {
         if (this.props.authenticated) {
             // show a link to sign out
-            return <li className = "nav-item" >
-                <
-                Link className = "nav-link"
-            to = "/feature" > Home < /Link> <
-                Link className = "nav-link"
-            to = "/signout" > Sign Out < /Link> <
-                /li>
-        } else {
+            return [
+              <li className = "nav-item" >
+              <Link className = "nav-link" to = "/feature"> Home </Link>
+              </li>,
+              <li className = "nav-item" >
+              <Link className = "nav-link" to = "/signout" > Sign Out < /Link>
+              </li>
+            ];
+          } else {
             // show a link to sign in or sign up
-            return [ <
-                li className = "nav-item"
-                key = { 1 } >
-                <
-                Link className = "nav-link"
-                to = "/signin" > Sign In < /Link> <
-                /li>, <
-                li className = "nav-item"
-                key = { 2 } >
-                <
-                Link className = "nav-link"
-                to = "/signup" > Sign Up < /Link> <
-                /li>
+            return [
+              <li className = "nav-item" key = { 1 } >
+                <Link className = "nav-link" to = "/signin" > Sign In < /Link>
+              </li>,
+              <li className = "nav-item" key = { 2 } >
+                <Link className = "nav-link" to = "/signup" > Sign Up < /Link>
+              </li>
             ];
         }
     }
